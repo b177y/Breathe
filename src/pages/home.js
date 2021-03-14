013c12a05788660308326c99cc7553e65a9ed0f1
b177y <34008579+b177y@users.noreply.github.com>
2021-03-14 15:52:29 +0000

    Last backup

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,26 +3,41 @@ import SettingsIcon from "../assets/settingsicon.svg";
 import {Link} from "react-router-dom";
 
 function HomePage(){
-    const [ inhale, setInhale ] = useState(7);
-    const [ hold, setHold ] = useState(7);
-    const [ exhale, setExhale ] = useState(7);
+    const [ inhale, setInhale ] = useState(2);
+    const [ hold, setHold ] = useState(2);
+    const [ exhale, setExhale ] = useState(2);
+    const [ duration, setDuration ] = useState(6);
+    const [ breatheWord, setBreatheWord ] = useState("Inhale");
     const cssAnimation = `
-    @keyframes breatheAnimation {
-        0% { opacity: 0; }
-        50% { opacity: 1; }
-        100% { opacity: 0; }
+    @keyframes breathe {
+        0% {
+             height: 600px
+             width: 600px;
+             font-size: 60px;
+        }
+        50% {
+             height: 200px
+             width: 200px;
+             font-size: 40px;
+        }
+        100% {
+             height: 600px
+             width: 600px;
+             font-size: 60px;
+        }
     }
     `
+
     return(
         <div className="screen">
             <style>{cssAnimation}</style>
             <div className="settingsButton"><Link to="/settings">
                 <img className="settingsIcon" src={SettingsIcon} alt="Settings" /></Link>
             </div>
-            <div className="mainsection circle" style={{animationDuration: 4}}>
-                <p className="circle-text">Hold</p>
+            <div className="mainsection circle" style={{animationDuration: `${duration}s`}}>
+                <p className="circle-text">{breatheWord}</p>
             </div>
-            <div className="footer"><p>Swipe for useful information >></p></div>
+            <div className="footer"><p>Tap here for useful information</p></div>
         </div>
         )
 }
